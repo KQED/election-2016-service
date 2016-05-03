4a0b58cf4ead961764a2d12e3101ec97efef0137
Cynthia Chen <cynthiaechen@gmail.com>
2016-05-03 14:47:40 -0700

    Add APresults database handler and modify processData function for consistency

--- server/handlers/apHandler.js
+++ server/handlers/apHandler.js
@@ -2,15 +2,14 @@ var async = require('async'),
     rp = require('request-promise'),
     log = require('../logging/bunyan'),
     processData = require('../utils/processData');
+    models = require('../../models');
 
 module.exports = {
   getJson: function(req, res) {
     var url = process.env.AP_URL + '&officeID=Z&officeID=P&officeID=H&officeID=Y';
-    
     rp(url).then(function(body){
       body = JSON.parse(body);
       var processedData = processData.processAp(body);
-
       res.send(processedData);
     }).catch(function(err){
       console.log(err);
@@ -19,6 +18,17 @@ module.exports = {
     });
   },
   getFromDataBase: function(req, res) {
-
+    models.APResults.findAll().then(function(results) {
+      var data = [];
+      results.forEach(function(result) {
+        if(result.dataValues.winner === 'X') {
+          result.dataValues.winner = true;
+        } else {
+          result.dataValues.winner = false;
+        }
+        data.push(result.dataValues);
+      });
+      res.send(data);
+    });
   }
 };
