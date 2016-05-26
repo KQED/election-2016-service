2a1a59ec1a9d773e5b5173c9f28ce138b9e53e3e
Sarah Yu <sarah.m.yu88@gmail.com>
2016-05-26 09:27:34 -0700

    Add endpoint to send local data, specifically restrict post/put/delete requests to server

--- server/handlers/googleHandler.js
+++ server/handlers/googleHandler.js
@@ -2,36 +2,7 @@ var GoogleSpreadsheet = require('google-spreadsheet'),
     sfgovConfig = require('../utils/sfgovConfig');
 
 module.exports = {
-  getRows: function(req, res) {
-    var sheet;
-    var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
-    doc.getInfo(function(err, info){
-      sheet = info.worksheets[0];
-      sheet.getCells({
-        'min-row': 1,
-        'max-row': 1,
-        'min-col': 2,
-        'max-col': 3,
-        'return-empty': false
-      }, function(err, cells) {
-                
-        sheet.getRows({
-          offset: 1,
-          limit: 20,
-          orderby: 'col2'
-        }, function(err, rows){
-          var jsonRows = rows.map(function(row){
-            var object = {location: row.location};
-            object[cells[0]._value] = row[cells[0]._value];
-            object[cells[1]._value] = row[cells[1]._value];
-            return object;
-          });
-          res.send(jsonRows);
-        });
-      });
-    });
-  },
-  getSfRows: function(req, res) {
+  getLocalRows: function(req, res) {
     var sheet;
     var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
     doc.getInfo(function(err, info){
@@ -46,10 +17,39 @@ module.exports = {
                 
         sheet.getRows({
           offset: 1,
+          limit: 200,
+          orderby: 'col2'
+        }, function(err, rows){
+          var jsonRows = rows.map(function(row){
+            delete row._xml;
+            delete row.id;
+            delete row._links;
+            row.counties = row.counties.split(",");
+            return row;
+          });
+          res.send(jsonRows);
+        });
+      });
+    });
+  },
+  getSfRows: function(req, res) {
+    var sheet;
+    var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
+    doc.getInfo(function(err, info){
+      sheet = info.worksheets[2];
+      sheet.getCells({
+        'min-row': 1,
+        'max-row': 1,
+        'min-col': 2,
+        'max-col': 3,
+        'return-empty': false
+      }, function(err, cells) {
+                
+        sheet.getRows({
+          offset: 1,
           limit: 300,
           orderby: 'col1'
         }, function(err, rows){
-          console.log(rows.length);
           var jsonRows = rows.map(function(row){
             delete row._xml;
             delete row.id;
--- server/utils/redisConfig.js
+++ server/utils/redisConfig.js
@@ -2,14 +2,14 @@ module.exports = {
   cache: {
       development: {
           enabled                 : true,
-          cache_expiry_seconds    : 60 * 5,
+          cache_expiry_seconds    : 60 * 1,
           redis_host              : 'localhost',
           redis_port              : 6379,
           prefix                  : 'debug' // If not defined, random is used
       },
       production: {
           enabled                 : true,
-          cache_expiry_seconds    : 60 * 5,
+          cache_expiry_seconds    : 60 * 3,
           redis_host              : process.env.REDIS_URL,
           redis_port              : 6379
       }
