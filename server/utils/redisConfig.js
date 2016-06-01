5c84740be0b5937344fa359caed60a2a217f6134
Sarah Yu <sarah.m.yu88@gmail.com>
2016-06-01 13:35:00 -0700

    Update to add more rows in sheet

--- server/utils/redisConfig.js
+++ server/utils/redisConfig.js
@@ -9,7 +9,7 @@ module.exports = {
       },
       production: {
           enabled                 : true,
-          cache_expiry_seconds    : 60 * 3,
+          cache_expiry_seconds    : 60 * 5,
           redis_host              : process.env.REDIS_URL,
           redis_port              : 6379
       }
