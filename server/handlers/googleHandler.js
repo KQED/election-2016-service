4901bd4676d8b666a46c2217f8082ef8272076e9
Cynthia Chen <cynthiaechen@gmail.com>
2016-10-19 10:52:14 -0700

    Comment out unused routes and handlers. Update SF Gov handler to new sheet

--- server/handlers/googleHandler.js
+++ server/handlers/googleHandler.js
@@ -43,90 +43,90 @@ module.exports = {
     });
   },
   //get Presidential and Senate
-  getPresSenateRows: function(req, res) {
-    var sheet;
-    var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
-    doc.getInfo(function(err, info){
-      sheet = info.worksheets[3];
-      sheet.getCells({
-        'min-row': 1,
-        'max-row': 1,
-        'min-col': 2,
-        'max-col': 3,
-        'return-empty': false
-      }, function(err, cells) {
+  // getPresSenateRows: function(req, res) {
+  //   var sheet;
+  //   var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
+  //   doc.getInfo(function(err, info){
+  //     sheet = info.worksheets[3];
+  //     sheet.getCells({
+  //       'min-row': 1,
+  //       'max-row': 1,
+  //       'min-col': 2,
+  //       'max-col': 3,
+  //       'return-empty': false
+  //     }, function(err, cells) {
                 
-        sheet.getRows({
-          offset: 1,
-          limit: 100,
-          orderby: 'col2'
-        }, function(err, rows){
-          var totalVotes = processData.calculateTotalPresSenateVotes(rows);
-          var jsonRows = rows.map(function(row){
-            var voteKey = processData.hashKey(row.officename);
-            if(row.officename === 'President') {
-              voteKey = processData.hashKey(row.officename, row.party);
-            }
-            //remove unwanted google doc metadata
-            delete row._xml;
-            delete row.id;
-            delete row._links;
-            //calculate percentage of votes based on total votes
-            row.votecount = parseInt(row.votecount);
-            row.votepercent = row.votecount / totalVotes[voteKey];
-            row.totalvotes = totalVotes[voteKey];
-            return row;
-          });
-          res.send(jsonRows);
-        });
-      });
-    });
-  },
+  //       sheet.getRows({
+  //         offset: 1,
+  //         limit: 100,
+  //         orderby: 'col2'
+  //       }, function(err, rows){
+  //         var totalVotes = processData.calculateTotalPresSenateVotes(rows);
+  //         var jsonRows = rows.map(function(row){
+  //           var voteKey = processData.hashKey(row.officename);
+  //           if(row.officename === 'President') {
+  //             voteKey = processData.hashKey(row.officename, row.party);
+  //           }
+  //           //remove unwanted google doc metadata
+  //           delete row._xml;
+  //           delete row.id;
+  //           delete row._links;
+  //           //calculate percentage of votes based on total votes
+  //           row.votecount = parseInt(row.votecount);
+  //           row.votepercent = row.votecount / totalVotes[voteKey];
+  //           row.totalvotes = totalVotes[voteKey];
+  //           return row;
+  //         });
+  //         res.send(jsonRows);
+  //       });
+  //     });
+  //   });
+  // },
   //get HouseAssembly
-  getHouseRows: function(req, res) {
-    var sheet;
-    var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
-    doc.getInfo(function(err, info){
-      sheet = info.worksheets[4];
-      sheet.getCells({
-        'min-row': 1,
-        'max-row': 1,
-        'min-col': 2,
-        'max-col': 3,
-        'return-empty': false
-      }, function(err, cells) {
+  // getHouseRows: function(req, res) {
+  //   var sheet;
+  //   var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
+  //   doc.getInfo(function(err, info){
+  //     sheet = info.worksheets[4];
+  //     sheet.getCells({
+  //       'min-row': 1,
+  //       'max-row': 1,
+  //       'min-col': 2,
+  //       'max-col': 3,
+  //       'return-empty': false
+  //     }, function(err, cells) {
                 
-        sheet.getRows({
-          offset: 1,
-          limit: 300,
-          orderby: 'col2'
-        }, function(err, rows){
-          var totalVotes = processData.calculateTotalPresSenateVotes(rows);
-          var jsonRows = rows.map(function(row){
-            var voteKey = processData.hashKey(row.officename, row.seatname);
-            //remove unwanted google doc metadata
-            delete row._xml;
-            delete row.id;
-            delete row._links;
-            //transform counties string into array
-            row.counties = row.counties !== '' ? row.counties.split(",") : '';
-            //calculate percentage of votes based on total votes
-            row.votecount = parseInt(row.votecount);
-            row.votepercent = row.votecount / totalVotes[voteKey];
-            row.totalvotes = totalVotes[voteKey];
-            return row;
-          });
-          res.send(jsonRows);
-        });
-      });
-    });
-  },
+  //       sheet.getRows({
+  //         offset: 1,
+  //         limit: 300,
+  //         orderby: 'col2'
+  //       }, function(err, rows){
+  //         var totalVotes = processData.calculateTotalPresSenateVotes(rows);
+  //         var jsonRows = rows.map(function(row){
+  //           var voteKey = processData.hashKey(row.officename, row.seatname);
+  //           //remove unwanted google doc metadata
+  //           delete row._xml;
+  //           delete row.id;
+  //           delete row._links;
+  //           //transform counties string into array
+  //           row.counties = row.counties !== '' ? row.counties.split(",") : '';
+  //           //calculate percentage of votes based on total votes
+  //           row.votecount = parseInt(row.votecount);
+  //           row.votepercent = row.votecount / totalVotes[voteKey];
+  //           row.totalvotes = totalVotes[voteKey];
+  //           return row;
+  //         });
+  //         res.send(jsonRows);
+  //       });
+  //     });
+  //   });
+  // },
   //get San Francisco government results (uploaded to google sheet)
   getSfRows: function(req, res) {
     var sheet;
     var doc = new GoogleSpreadsheet(process.env.GOOGLE_DOCS_KEY);
     doc.getInfo(function(err, info){
-      sheet = info.worksheets[2];
+      sheet = info.worksheets[1];
       sheet.getCells({
         'min-row': 1,
         'max-row': 1,
@@ -146,7 +146,7 @@ module.exports = {
             delete row._links;
             row.precincts = row.processeddone / row.totalprecincts;
             row.votepercent = row.total / row.contesttotal;
-            row.propdescription = sfgovConfig.sfgovDescription[row.contestfullname] ? sfgovConfig.sfgovDescription[row.contestfullname] : '';
+            // row.propdescription = sfgovConfig.sfgovDescription[row.contestfullname] ? sfgovConfig.sfgovDescription[row.contestfullname] : '';
             return row;
           //filter to only return rows of desired races
           }).filter(module.exports.filterRows);
