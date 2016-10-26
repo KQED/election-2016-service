5a7ee3aea33bb4c830a854f3f3bfece190bbcdbc
Cynthia Chen <cynthiaechen@gmail.com>
2016-10-26 15:10:26 -0700

    Fix errors in sfconfig file

--- server/utils/sfgovConfig.js
+++ server/utils/sfgovConfig.js
@@ -30,13 +30,12 @@ module.exports = {
       "District 17": ["Alameda",  "Santa Clara"],
       "District 18": ["San Mateo", "Santa Clara", "Santa Cruz"],
       "District 19": ["Santa Clara"],
-      "District 20": ["Monterey", "Santa Clara", "Santa Cruz"]
+      "District 20": ["Monterey", "San Benito", "Santa Clara", "Santa Cruz"]
     },
     "State Senate": {
       "District 3": ["Contra Costa", "Napa", "Sacramento", "Solano", "Sonoma", "Yolo"],
       "District 7": ["Alameda",  "Contra Costa"],
       "District 9": ["Alameda",  "Contra Costa"],
-      "District 10": ["Alameda", "Santa Clara"],
       "District 11": ["San Francisco",  "San Mateo"],
       "District 13": ["San Mateo",  "Santa Clara"],
       "District 15": ["Santa Clara"],
@@ -48,7 +47,7 @@ module.exports = {
       "District 10": ["Marin",  "Sonoma"],
       "District 11": ["Contra Costa", "Sacramento", "Solano"],
       "District 14": ["Contra Costa",  "Solano"],
-      "District 15": ["Alameda"],
+      "District 15": ["Alameda",  "Contra Costa"],
       "District 16": ["Alameda",  "Contra Costa"],
       "District 17": ["San Francisco"],
       "District 18": ["Alameda"],
@@ -68,7 +67,7 @@ module.exports = {
     "U.S. House": ["District 2", "District 3", "District 5", "District 11",
     "District 12", "District 13", "District 14", "District 15", "District 17",
     "District 18", "District 19", "District 20"],
-    "State Senate": ["District 3", "District 7", "District 9", "District 10", "District 11",
+    "State Senate": ["District 3", "District 7", "District 9", "District 11",
     "District 13", "District 15", "District 17"],
     "State Assembly": ["District 2", "District 4", "District 10", "District 11", 
     "District 14", "District 15", "District 16", "District 17", "District 18",
