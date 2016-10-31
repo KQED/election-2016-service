aa28df3ddd09e39321a38b76c928f4df048a7d5a
Cynthia Chen <cynthiaechen@gmail.com>
2016-10-31 11:44:30 -0700

    Fix officename and seatname for certain SF races

--- server/handlers/googleHandler.js
+++ server/handlers/googleHandler.js
@@ -162,8 +162,13 @@ module.exports = {
             delete row.id;
             delete row._links;
             var raceName = htmlParser.splitByComma(row.contestfullname);
-            row.officename = raceName[0];
-            row.seatname = raceName[1];
+            if(raceName[0].indexOf('Member') > -1) {
+              row.officename = raceName[1];
+              row.seatname = raceName[0];
+            } else {
+              row.officename = raceName[0];
+              row.seatname = raceName[1];              
+            }
             row.fullname = htmlParser.formatChoicename(row.candidatefullname);
             row.precinctsReportingPct = row.processeddone / row.totalprecincts;
             row.votepercent = row.total / row.contesttotal;
